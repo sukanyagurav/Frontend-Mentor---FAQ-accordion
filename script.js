5d7407a5c48aba436453bd43f2a50b3ebead3ff6
sukanya gurav <sukanyagurav6@gmail.com>
2024-02-22 15:46:23 +0530

    completed

--- script.js
+++ script.js
@@ -8,15 +8,18 @@ for(let i=0;i<btns.length;i++){
         if(parseInt(contentDiv[i].style.height) != contentDiv[i].scrollHeight){
             contentDiv[i].style.height = contentDiv[i].scrollHeight + 'px'
             btns[i].classList.add('active')
+            btns[i].setAttribute('aria-expanded',false)
         }
         else{
             contentDiv[i].style.height =  '0px'
             btns[i].classList.remove('active')
+            btns[i].setAttribute('aria-expanded',true)
         }
         for (let j = 0; j < contentDiv.length; j++) {
 			if (j != i) {
                 contentDiv[j].style.height =  '0px'
                 btns[j].classList.remove('active')
+                btns[j].setAttribute('aria-expanded',true)
 			}
 		}
     })
